0e977a5eec9403b121be1a56bbc1d56ac5a7974f
s76 <sheepw76@gmail.com>
2015-10-28 15:09:57 +0100

    well, steering works again with multi dynamic objects..

--- PathFindingTest/Assets/MySpace/Scripts/Steering2.cs
+++ PathFindingTest/Assets/MySpace/Scripts/Steering2.cs
@@ -12,63 +12,77 @@ public class Steering2 : MonoBehaviour
 	public float detect_range;
 	public float conner_detect_range;
 	public float strength = 2;
-	
-	public Transform left_point, right_point;
-	
+
 	Vector3 dir;
-
-	void CornerCheck (Vector3 tmp01, RaycastHit hit) {
-
-	}
 
 	bool keep_side;		
 
 	Vector3 turn_side;
 
+	Vector3 Vec90 ( Vector3 first , Vector3 normal ) {
+		Vector3 left90 = Quaternion.Euler (0, -90, 0) * first;
+		left90.Normalize ();
+		if (Vector3.Dot (left90, normal) > 0)
+			return left90;
+		return (Quaternion.Euler (0, 90, 0) * first).normalized;
+	}
 	public void Process (float delta ) {
 		dir = transform.forward;
 		
-		Vector3 left45 = Quaternion.Euler(0, -45, 0) * dir* detect_range;
-		Vector3 right45 = Quaternion.Euler(0, 45, 0) * dir* detect_range;
+		Vector3 left45 = Quaternion.Euler(0, -45, 0) * dir* detect_range*0.75f;
+		Vector3 right45 = Quaternion.Euler(0, 45, 0) * dir* detect_range*0.75f;
 		
 		
 		Vector3 detect_ray = dir * detect_range;
 		
 		RaycastHit hit, left_hit, right_hit;
+		
+		Debug.DrawLine (transform.position, transform.position + detect_ray, Color.magenta);
 
-		if (Physics.Linecast (transform.position, transform.position + detect_ray, out hit)) {
-	
-			
-			Vector3 steer_des = Vector3.zero;
+		Vector3 main_steer = Vector3.zero;
+		Vector3 left_steer = Vector3.zero;
+		Vector3 right_steer = Vector3.zero;
 
-			bool r, l;
-			if (l = Physics.Linecast (transform.position, transform.position + left45, out left_hit)) {
-				if (!keep_side)
-					turn_side = -transform.right;
-				steer_des = hit.point + (left_hit.point - hit.point).normalized * strength;
-				
-				Debug.DrawLine (hit.point, hit.point + steer_des, Color.green);
-			} 
-			if (r = Physics.Linecast (transform.position, transform.position + right45, out right_hit)) {
-				if (!keep_side)
-					turn_side = transform.right;			
-				steer_des = hit.point + (left_hit.point - hit.point).normalized * strength;
-				Debug.DrawLine (hit.point, hit.point + steer_des, Color.green);
-			} 
+		turn_side = transform.right;
 
-			if ( !r & !l) {
-				steer_des = hit.point + Degree90LineCast (transform.position - hit.point, turn_side).normalized * strength;
-				Debug.DrawLine (hit.point, hit.point + steer_des, Color.green);
+		bool r = false, l= false;
+		if (l = Physics.Linecast (transform.position, transform.position + left45, out left_hit)) {
+			if (!keep_side) { 
+				keep_side = true;
+				turn_side = transform.right;
 			}
 
-			dir = (steer_des - transform.position).normalized;
-		} else {
+			left_steer = Vec90(left_hit.point - transform.position,transform.forward) * 0.75f * strength ;
+			
+			Debug.DrawLine (transform.position,left_hit.point , Color.red );
+			Debug.DrawLine (transform.position,transform.position + Vec90(left_hit.point - transform.position,transform.forward));
+		} else if ( r = Physics.Linecast (transform.position, transform.position + right45, out right_hit)) {
+			if (!keep_side) {
+				keep_side = true;
+				turn_side = -transform.right;	
+			}
+			right_steer = ReflectLineCast(right_hit.point - transform.position,transform.forward).normalized * 0.75f * strength ;
+		} 
+
+		if (Physics.Linecast (transform.position, transform.position + detect_ray, out hit)) {
+			main_steer = turn_side;
+		} 
+
+		if ( !r & !l) {
 			keep_side = false;
 		}
-		
+
+		//Debug.DrawLine (transform.position,transform.position+ dir);
+		//Debug.DrawLine (transform.position,transform.position+ main_steer);
+		//Debug.DrawLine (transform.position,transform.position+ left_steer);
+		//Debug.DrawLine (transform.position,transform.position+ right_steer);
+
+		dir = (dir+ main_steer + left_steer + right_steer).normalized;
+
+	//	Debug.DrawLine (transform.position, transform.position + dir, Color.yellow);
+	
 		var q = Quaternion.LookRotation(dir);
-		
-		Debug.DrawLine (transform.position, transform.position + dir, Color.yellow);
+
 		transform.rotation = Quaternion.Slerp(transform.rotation,q,turn_speed*Time.deltaTime);
 		transform.position += dir*speed*Time.deltaTime;
 		
@@ -87,22 +101,7 @@ public class Steering2 : MonoBehaviour
 		if ( k > 0 ) return v1;
 		else return new Vector3(-u.x,first.y,u.y);
 	}
-	
-	Vector3 Degree90LineCast ( Vector3 first, Vector3 normal ) {
-		Vector3 u = first.normalized;
-		float angle = AngleOfNormalized(u);
-		
-		if (  u.y >=0 ) {
-			var r= SetAngle(angle + 90);
-			if ( Vector3.Dot(normal,r ) > 0 ) return r;
-			else return SetAngle(angle-90);
-		}else {
-			var r= SetAngle(-angle + 90);
-			if ( Vector3.Dot(normal,r ) > 0 ) return r;
-			else return SetAngle(-angle-90);
-		}
-	}
-	
+
 	float AngleOfNormalized (Vector3 normalized ) {
 		return Mathf.Acos(normalized.x)*Mathf.Rad2Deg;
 	}
--- PathFindingTest/Assets/TestNavMove.cs
+++ PathFindingTest/Assets/TestNavMove.cs
@@ -6,14 +6,11 @@ using React;
 using Action = System.Collections.Generic.IEnumerator<React.NodeResult>;
 
 public class TestNavMove : MonoBehaviour {
-	public Vector3 off;
 	Transform target;
 	NavMeshAgent agent;
-	Reactor reactor;
 
 	void Awake () {
 		agent = GetComponent<NavMeshAgent> ();
-		reactor = GetComponent<Reactor> ();
 		target = GameObject.Find ("target").transform;
 	}
 
@@ -22,138 +19,16 @@ public class TestNavMove : MonoBehaviour {
 		agent.SetDestination (target.position);
 	}
 
-	public enum MState { MoveToTarget, MoveAround }
-	public MState state;
-
-	public void MoveToTarget () {
-		agent.SetDestination (target.position);
-		state = MState.MoveToTarget;
-	}
-
-	public void MoveAround () {
-		NavMeshPath path = new NavMeshPath();
-		agent.CalculatePath(temp_des, path);
-	
-		if (path.status == NavMeshPathStatus.PathPartial) {
-			MoveToTarget ();
-			Debug.Log("MoveAround1 ");
-		} else {
-			Debug.Log("MoveAround2");
-			agent.SetPath (path);
-		}
-
-		state = MState.MoveAround;
-	}
-
-	public bool IsMoveToTarget () {
-		return state == MState.MoveToTarget;
-	}
-
-	public bool IsMoveAround () {
-		return state == MState.MoveAround;
-	}
-
-	public float check_range = 1f;
-	float timer = 0;
-
-	public float des_tolerance = 1f;
-	Vector3 temp_des;
-
-	public float move_around_len = 2f;
-
-	public bool IsArrivedAtDes () {
-	//	if ( IsMoveAround () ) { 
-
-		if ((transform.position - temp_des).sqrMagnitude > des_tolerance) {
-			Debug.Log("move to tmp");
-			return true;
-		} else {
-			Debug.Log("Reached temp des");
-			agent.SetDestination(target.position);
-			return false;
-		}
-		//}
-	}
-
-	enum Dir { left, right , back, forw}
-	Dir lastDir; 
-
-	public float check_freq;
-
-	public Action Evaluate () {
-		yield return NodeResult.Continue;
-		Debug.Log ("Eval");
-		timer += reactor.TickDuration;
-		if (timer > check_freq) {
-			timer = 0; 
-			Vector3 fw = transform.forward;
-			Vector3 left = Quaternion.Euler(0, -60, 0) * fw;
-			Vector3 right = Quaternion.Euler(0, 60, 0) * fw;
-
-			bool _forw=false, _left=false , _right=false;
-			
-			RaycastHit hit_left, hit_right, hit_front ;
-			if (_forw = Physics.Raycast(transform.position, fw,out hit_front , check_range ) ) {
-				Debug.Log("hit front");
-				_left = Physics.Raycast(transform.position, left, out hit_left, check_range *0.5f );
-				_right = Physics.Raycast(transform.position, right,  out hit_right, check_range *0.5f);
-			}
-
-			if ( _left & _right ) {
-				
-				Debug.Log("should go back now");
-				temp_des = transform.position +  -(hit_front.transform.position - transform.position).normalized*move_around_len;
-				lastDir = Dir.back;
-			} else if ( _left ) {
-				Debug.Log("should turn right now");
-				temp_des = transform.position + Quaternion.Euler(0, 90, 0) * (hit_left.transform.position - transform.position).normalized * move_around_len;
-				lastDir = Dir.right;
-			} else if ( _right ) {
-				Debug.Log("should turn left now");
-				temp_des = transform.position + Quaternion.Euler(0, -90, 0) * (hit_right.transform.position - transform.position).normalized * move_around_len;
-				lastDir = Dir.left;
-			} 
-
-			if ( _forw & ( !_left & !_right ) ) { 
-				if ( lastDir == Dir.left ) {
-					temp_des = transform.position + Quaternion.Euler(0, -90, 0) * (hit_front.transform.position - transform.position).normalized * move_around_len;
-				}
-				if ( lastDir == Dir.right ) {
-					temp_des = transform.position + Quaternion.Euler(0, 90, 0) * (hit_front.transform.position - transform.position).normalized * move_around_len;
-				}
-				if ( lastDir == Dir.back ) {
-					temp_des = transform.position + -(hit_front.transform.position - transform.position).normalized*move_around_len;
-				}
-
-			}
-
-			if ( _forw ) {
-				MoveAround();
-			} else {
-				MoveToTarget();
-			}
-
-		}
-		yield return NodeResult.Continue;
+	void Update () {
+		Debug.Log ("agent.pathPending=" + agent.pathPending);
+		Debug.Log ("agent.pathStatus=" + agent.pathStatus);
+		Debug.Log ("agent.isPathStale=" + agent.isPathStale);
+		Debug.Log ("agent.hasPath=" + agent.hasPath);
+		Debug.Log ("agent.pathEndPosition=" + agent.pathEndPosition);
+		Debug.Log ("agent.destination=" + agent.destination);
 	}
 
 	void OnDrawGizmos () {
-		if (IsMoveAround ()) {
-			Gizmos.DrawSphere( temp_des,0.2f);
-		}
-		UnityEditor.Handles.color = Color.cyan;
-		UnityEditor.Handles.DrawWireDisc (transform.position, transform.up, des_tolerance);
-
-		Vector3 fw = transform.forward;
-		Vector3 left = Quaternion.Euler(0, -60, 0) * fw;
-		Vector3 right = Quaternion.Euler(0, 60, 0) * fw;
-
-		Gizmos.color = Color.yellow;
-		Gizmos.DrawLine ( transform.position, transform.position + fw* check_range);
-		Gizmos.color = Color.green;
-		Gizmos.DrawLine ( transform.position, transform.position + left* check_range *0.5f);
-		Gizmos.color = Color.magenta;
-		Gizmos.DrawLine ( transform.position, transform.position + right* check_range *0.5f);
 
 		if (agent != null) {
 			if (agent.hasPath) {
